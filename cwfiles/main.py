8cea991f74cd7ee0f3209b5370d33a47cdf441cf
Olivier Roques <olivier.roques@telecom-paristech.fr>
2018-11-13 12:18:29 +0000

    Add book and write a test

--- cwfiles/main.py
+++ cwfiles/main.py
@@ -5,48 +5,51 @@
 import json	# load
 import sys	# argv
 
-import ot	# alice, bob
 import util	# ClientSocket, log, ServerSocket
-import yao	# Circuit
+# import ot	# alice, bob
+# import yao	# Circuit
 
 # Alice is the circuit generator (client) __________________________________
 
 def alice(filename):
-  socket = util.ClientSocket()
+    socket = util.ClientSocket()
 
-  with open(filename) as json_file:
-    json_circuits = json.load(json_file)
+    with open(filename) as json_file:
+        json_circuits = json.load(json_file)
 
-  for json_circuit in json_circuits['circuits']:
-    # << removed >>
+    for json_circuit in json_circuits['circuits']:
+        pass
+        # << removed >>
 
 # Bob is the circuit evaluator (server) ____________________________________
 
 def bob():
-  socket = util.ServerSocket()
-  util.log(f'Bob: Listening ...')
-  while True:
-    # << removed >>
+    socket = util.ServerSocket()
+    util.log(f'Bob: Listening ...')
+    while True:
+        pass
+        # << removed >>
 
 # local test of circuit generation and evaluation, no transfers_____________
 
 def local_test(filename):
-  with open(filename) as json_file:
-    json_circuits = json.load(json_file)
+    with open(filename) as json_file:
+        json_circuits = json.load(json_file)
 
-  for json_circuit in json_circuits['circuits']:
-    # << removed >>
+    for json_circuit in json_circuits['circuits']:
+        pass
+        # << removed >>
 
 # main _____________________________________________________________________
 
 def main():
-  behaviour = sys.argv[1]
-  if   behaviour == 'alice': alice(filename=sys.argv[2])
-  elif behaviour == 'bob':   bob()
-  elif behaviour == 'local': local_test(filename=sys.argv[2])
+    behaviour = sys.argv[1]
+    if   behaviour == 'alice': alice(filename=sys.argv[2])
+    elif behaviour == 'bob':   bob()
+    elif behaviour == 'local': local_test(filename=sys.argv[2])
 
 if __name__ == '__main__':
-  main()
+    main()
 
 # __________________________________________________________________________
 
