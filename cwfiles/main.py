a58a507e3f449ff5332a35292347d6f8d8a09542
Olivier Roques <olivier.roques@telecom-paristech.fr>
2018-11-17 17:07:55 +0000

    Rename garble into yao

--- cwfiles/main.py
+++ cwfiles/main.py
@@ -6,14 +6,12 @@ Olivier Roques    : or518
 # yao garbled circuit evaluation v1. simple version based on smart
 # naranker dulay, dept of computing, imperial college, october 2018
 
-import json	# load
-import sys	# argv
+import json         # load
+import sys          # argv
 
-import util	# ClientSocket, log, ServerSocket
-import ot	# alice, bob
-import yao	# Circuit
-
-import garble as gb
+import util         # ClientSocket, log, ServerSocket
+import ot           # alice, bob
+import yao          # Circuit
 
 # Alice is the circuit generator (client) __________________________________
 
@@ -25,7 +23,7 @@ def alice(filename):
 
     for json_circuit in json_circuits['circuits']:
         outputs   = json_circuit["out"]
-        g_circuit = gb.GarbledCircuit(json_circuit)
+        g_circuit = yao.GarbledCircuit(json_circuit)
         g_tables  = g_circuit.get_garbled_tables()
         keys      = g_circuit.get_keys()
         pbits     = g_circuit.get_pbits()
@@ -58,7 +56,7 @@ def print_evaluation(circuit, g_tables, keys, pbits):
                 (keys[(b_wires[i], bits_b[i])], pbits[b_wires[i]] ^ bits_b[i])
 
         # TODO: evaluation is on Bob's side
-        evaluation = gb.evaluate(circuit, g_tables, pbits_out, a_inputs, b_inputs)
+        evaluation = yao.evaluate(circuit, g_tables, pbits_out, a_inputs, b_inputs)
 
         str_bits_a     = ' '.join(bits[:len_a_wires]) + ' '*bool(len_a_wires)
         str_bits_b     = ' '.join(bits[len_a_wires:]) + ' '*bool(len_b_wires)
