c09b150011c091953533c76eddcad14cb9445ead
Olivier Roques <olivier.roques@telecom-paristech.fr>
2018-11-18 01:39:02 +0000

    Write bob side

--- cwfiles/main.py
+++ cwfiles/main.py
@@ -22,21 +22,22 @@ def alice(filename):
         json_circuits = json.load(json_file)
 
     for json_circuit in json_circuits['circuits']:
-        outputs   = json_circuit["out"]
         g_circuit = yao.GarbledCircuit(json_circuit)
         g_tables  = g_circuit.get_garbled_tables()
         keys      = g_circuit.get_keys()
         pbits     = g_circuit.get_pbits()
-        print_evaluation(json_circuit, g_tables, keys, pbits)
+        pbits_out = {w: pbits[w] for w in json_circuit["out"]}
+        og.send_yao_circuit(socket, json_circuit, g_tables, pbits_out)
+        print_evaluation(socket, json_circuit, keys, pbits)
     print()
 
-def print_evaluation(circuit, g_tables, keys, pbits):
+def print_evaluation(socket, circuit, keys, pbits):
     outputs   = circuit["out"]
     a_wires   = circuit.get("alice", [])
-    b_wires   = circuit.get("bob", [])
     a_inputs  = {}
-    b_inputs  = {}
-    pbits_out = {w: pbits[w] for w in outputs}
+    b_wires   = circuit.get("bob", [])
+    b_keys    = {(w, bit): (key, bit ^ pbits[w])\
+                 for (w, bit), key in keys.items() if w in b_wires}
 
     print("\n======= {0} =======".format(circuit["name"]))
 
@@ -50,21 +51,52 @@ def print_evaluation(circuit, g_tables, keys, pbits):
         for i in range(len_a_wires):
             a_inputs[a_wires[i]] = \
                 (keys[(a_wires[i], bits_a[i])], pbits[a_wires[i]] ^ bits_a[i])
-        for i in range(len_b_wires):
-            # TODO: retrieve bob's keys instead
-            b_inputs[b_wires[i]] = \
-                (keys[(b_wires[i], bits_b[i])], pbits[b_wires[i]] ^ bits_b[i])
 
-        # TODO: evaluation is on Bob's side
-        evaluation = yao.evaluate(circuit, g_tables, pbits_out, a_inputs, b_inputs)
+        result = ot.get_bob_result(socket, a_inputs, b_keys, pbits)
 
-        str_bits_a     = ' '.join(bits[:len_a_wires]) + ' '*bool(len_a_wires)
-        str_bits_b     = ' '.join(bits[len_a_wires:]) + ' '*bool(len_b_wires)
-        str_evaluation = ' '.join([str(v) for v in evaluation.values()]) + ' '
-        line           = "  Alice{0} = {1} Bob{2} = {3}  Outputs{4} = {5}".\
+        str_bits_a = ' '.join(bits[:len_a_wires]) + ' '*bool(len_a_wires)
+        str_bits_b = ' '.join(bits[len_a_wires:]) + ' '*bool(len_b_wires)
+        str_result = ' '.join([str(result[w]) for w in outputs]) + ' '
+        line       = "  Alice{0} = {1} Bob{2} = {3}  Outputs{4} = {5}".\
             format(a_wires, str_bits_a, b_wires, str_bits_b, \
-                   outputs, str_evaluation)
+                   outputs, str_result)
         print(line)
+
+# def print_evaluation(socket, circuit, g_tables, keys, pbits):
+#     outputs   = circuit["out"]
+#     a_wires   = circuit.get("alice", [])
+#     b_wires   = circuit.get("bob", [])
+#     a_inputs  = {}
+#     b_inputs  = {}
+#     pbits_out = {w: pbits[w] for w in outputs}
+
+#     print("\n======= {0} =======".format(circuit["name"]))
+
+#     len_a_wires, len_b_wires = len(a_wires), len(b_wires)
+#     N = len_a_wires + len_b_wires
+
+#     for bits in [format(n, 'b').zfill(N) for n in range(2**N)]:
+#         bits_a = [int(b) for b in bits[:len_a_wires]]
+#         bits_b = [int(b) for b in bits[len_a_wires:]]
+
+#         for i in range(len_a_wires):
+#             a_inputs[a_wires[i]] = \
+#                 (keys[(a_wires[i], bits_a[i])], pbits[a_wires[i]] ^ bits_a[i])
+#         for i in range(len_b_wires):
+#             # TODO: retrieve bob's keys instead
+#             b_inputs[b_wires[i]] = \
+#                 (keys[(b_wires[i], bits_b[i])], pbits[b_wires[i]] ^ bits_b[i])
+
+#         # TODO: evaluation is on Bob's side
+#         evaluation = yao.evaluate(circuit, g_tables, pbits_out, a_inputs, b_inputs)
+
+#         str_bits_a     = ' '.join(bits[:len_a_wires]) + ' '*bool(len_a_wires)
+#         str_bits_b     = ' '.join(bits[len_a_wires:]) + ' '*bool(len_b_wires)
+#         str_evaluation = ' '.join([str(evaluation[w]) for w in b_wires]) + ' '
+#         line           = "  Alice{0} = {1} Bob{2} = {3}  Outputs{4} = {5}".\
+#             format(a_wires, str_bits_a, b_wires, str_bits_b, \
+#                    outputs, str_evaluation)
+#         print(line)
 
 # Bob is the circuit evaluator (server) ____________________________________
 
@@ -72,7 +104,24 @@ def bob():
     socket = util.ServerSocket()
     util.log(f'Bob: Listening ...')
     while True:
-        pass
+        circuit, g_tables, pbits_out = og.receive_yao_circuit(socket)
+        send_evaluation(circuit, g_tables, pbits_out)
+
+def send_evaluation(socket, circuit, g_tables, pbits_out):
+    outputs   = circuit["out"]
+    a_wires   = circuit.get("alice", [])
+    b_wires   = circuit.get("bob", [])
+
+    len_a_wires, len_b_wires = len(a_wires), len(b_wires)
+    N = len_a_wires + len_b_wires
+
+    for bits in [format(n, 'b').zfill(N) for n in range(2**N)]:
+        bits_a = [int(b) for b in bits[:len_a_wires]]
+        bits_b = [int(b) for b in bits[len_a_wires:]]
+
+        b_inputs_clear = {b_wires[i]: bits_b[i] for i in range(len_b_wires)}
+        og.send_result(b_inputs_clear)
+
 
 # local test of circuit generation and evaluation, no transfers_____________
 
--- cwfiles/ot.py
+++ cwfiles/ot.py
@@ -3,18 +3,75 @@ Emmanuelle Risson : ear
 Olivier Roques    : or518
 """
 
+import util
+import yao
+
 # yao garbled circuit evaluation v1. simple version based on smart
 # naranker dulay, dept of computing, imperial college, october 2018
 
 OBLIVIOUS_TRANSFERS = True
 
 if OBLIVIOUS_TRANSFERS: # __________________________________________________
-    pass
+    def send_yao_circuit(socket, circuit, g_tables, pbits_out):
+        socket.send(circuit)
+        socket.send(g_tables)
+        socket.send(pbits_out)
+
+    def get_bob_result(socket, a_inputs, b_keys):
+        """
+        a_inputs of the form:
+            {wire: (key, encrypted_bit), ...}
+        b_keys of the form:
+            {(wire, 0): (key, 0^pbits[wire],
+             (wire, 1): key, 1^pbits[wire]...}
+        """
+        socket.send(a_inputs)
+
+        for i in range(len(b_keys) // 2):
+            w = socket.receive()
+            pair = (b_keys[w, 0], b_keys[w, 1])
+            ot_alice(socket, pair)
+
+        result = socket.receive()
+        return result
+
+    def ot_alice(socket, pair):
+        """
+            pair = (msg1, msg2)
+        """
+        pass
+
+    def receive_yao_circuit(socket):
+        circuit   = socket.receive()
+        g_tables  = socket.receive()
+        pbits_out = socket.receive()
+        return (circuit, g_tables, pbits_out)
+
+    def send_result(socket, circuit, g_tables, pbits_out, b_inputs):
+        """
+        b_inputs of the form:
+            {wire: clear_bit, ...}
+        """
+        a_inputs      = socket.receive()
+        b_inputs_encr = {}
+
+        for w, b_input in b_inputs.items():
+            socket.send(w)
+            ot_bot(socket, b_input)
+            b_inputs_encr[w] = socket.receive()
+
+        result = yao.evaluate(circuit, g_tables, pbits_out, a_inputs, b_inputs_encr)
+        socket.send(result)
+
+    def ot_bob(socket, b_input):
+        """
+            b_input = 0 or 1
+        """
+        pass
 
   # bellare-micali OT with naor and pinkas optimisations, see smart p423
 
 else: # ____________________________________________________________________
-    pass
 
   # non oblivious transfers, not even a secure channel is used, for testing
 
